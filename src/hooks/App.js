55ce25b8b93623ee77fc11f75e81c922d786731b
yan_cy <yan_cy@Ctrip.com>
2019-04-24 13:51:35 +0800

    接入useMemo

--- src/hooks/App.js
+++ src/hooks/App.js
@@ -1,16 +1,50 @@
-import React, { useState, useEffect } from 'react'
+import React, { Component, useState, useEffect, useMemo } from 'react'
 
-export default () => {
+const Com = () => {
+
     const [count, setCount] = useState(0);
 
     useEffect(() => {
         console.log('count: ', count)
     })
 
+    console.log('render 2')
     return <div>
         <p>You clicked { count } times</p>
         <button onClick={() => setCount(count + 1)}>
             Click me
         </button>
     </div>
+}
+
+const Index = () => {
+
+    console.log('render 1')
+
+    return useMemo(() => {
+        return <Com />
+    }, [])
+    
+}
+
+export default class extends Component {    
+
+    state = {
+        index: 0
+    }
+
+    componentDidMount() {
+        setInterval(() => {
+            this.setState({
+                index: this.state.index + 1
+            })
+        }, 1000);
+    }
+
+    render() {
+        return <div>
+            <Index />
+            <p>index: {this.state.index}</p>
+        </div>
+    }
 }
